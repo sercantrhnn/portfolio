abcaf628bc83e6aa09f4e47d2b147e86e8dc3d5e
sercan <sercann.turhan@gmail.com>
2024-12-13 15:32:57 +0300

    image edit

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -57,7 +57,7 @@ export default function Hero() {
                 initial={{ x: 100, opacity: 0}}
                 animate={{ x: 0, opacity:1 }}
                 transition={{duration: 1, delay: 1.2}}
-                className="rounded-2xl" src={profilePic} alt="Sercan Turhan"/>
+                className="rounded-full" src={profilePic} alt="Sercan Turhan"/>
             </div>
         </div>
       </div>
